dfafc70140cf7696c28d6a491e454b83b2ace533
josefortyfive <jose.fortyfive@gmail.com>
2023-12-20 21:42:06 +0800

    InsertionSort
    Insert

---
+++ src/test/InsertionSort.java
@@ -0,0 +1,36 @@
+package test;
+
+public class InsertionSort {
+    
+    public static void insertionSort(int arr[])
+    {
+        int n = arr.length;
+        
+        for(int i = 0; i < n; i++){
+            int key = arr[i];
+            int j = i - 1;
+            
+            while(j >= 0 && arr[j] > key){
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            
+            arr[j + 1] = key;
+        }
+    
+    }
+    
+    public static void printArray( int arr[]){
+        int n = arr.length;
+        
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]+" ");
+        }
+    }
+    public static void main(String args[]){
+        
+        int arr[] = { 4, 3,8,1,5};
+        insertionSort(arr);
+        printArray(arr);
+    }
+}
