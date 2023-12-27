87fd5994969f39c5b1f960ddacbc077a712d88cf
josefortyfive <jose.fortyfive@gmail.com>
2023-12-27 12:06:36 +0800

    Pyramid
    Pyramid

---
+++ src/random/Pyramid.java
@@ -0,0 +1,27 @@
+
+public class Pyramid {
+    
+    public static void main(String args[])
+    {
+        int rows = 5;
+        int k = 0;
+        
+        for(int i = 1; i <= rows; i++)
+        {
+            for(int space = 1; space <= rows - i; space++)
+            {
+                System.out.print(" ");
+            }
+            
+            k = 0;
+            
+            while(k != 2 * i - 1)
+            {
+                System.out.print("* ");
+                ++k;
+            }
+            
+            System.out.println();
+        }
+    }
+}
