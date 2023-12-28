93c5a4c14744c9fb177bc81e16c0d80f7c1902e2
josefortyfive <jose.fortyfive@gmail.com>
2023-12-28 18:19:55 +0800

    Rectangle
    Rectangle

---
+++ src/random/Rectangle.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Rectangle {
+    
+    private int x;
+    private int y;
+    
+    public Rectangle(){
+        this(0, 0);
+    }
+    
+    public Rectangle(int x, int y){
+        this.x = x;
+        this.y = y ;
+    }
+}
