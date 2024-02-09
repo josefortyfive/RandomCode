c039e41a7700309fa6dc1b73a92d2bb0792577de
josefortyfive <jose.fortyfive@gmail.com>
2024-02-09 20:28:19 +0800

    added the LinkedList challenge

---
+++ src/list/linkedlistchallenge/LinkedListChallenge.java
@@ -0,0 +1,59 @@
+package list.linkedlistchallenge;
+
+import java.util.LinkedList;
+
+record Place(String name, int distance) {
+	
+	@Override
+	public String toString() {
+		return String.format("%s (%d)", name, distance);
+	}
+	
+}
+
+public class LinkedListChallenge {
+
+	public static void main(String args[]) {
+		LinkedList<Place> placesToVisit = new LinkedList<>();
+		Place adelaide = new Place("Adelaide",1374);
+		addPlace(placesToVisit, adelaide);
+		addPlace(placesToVisit, new Place("adelaide", 1374));
+		addPlace(placesToVisit, new Place("Brisbane", 917));
+		addPlace(placesToVisit, new Place("Perth", 3923));
+		addPlace(placesToVisit, new Place("Alice Springs", 2771));
+		addPlace(placesToVisit, new Place("Darwin", 3972));
+		addPlace(placesToVisit, new Place("Melbourne", 877));
+		
+		placesToVisit.addFirst(new Place("Sydney", 0));
+		System.out.println(placesToVisit);
+		
+		
+		
+	}
+	
+	private static void addPlace(LinkedList<Place> list, Place place) {
+		if(list.contains(place)) {
+			System.out.println("Found the duplicate: "+place);
+			return;
+		}
+		
+		for(Place p : list) {										// check the list
+			if(p.name().equalsIgnoreCase(place.name())) {			// check if the name is the same	
+				System.out.println("Found duplicate "+place);		// if found shows duplicate message
+				return;
+			}
+		}
+		
+		int matchIndex = 0;
+		for(var listPlace : list) {
+			if(place.distance() < listPlace.distance()) {
+				list.add(matchIndex, place);
+				return;
+			}
+			matchIndex++;
+		}
+		list.add(place);
+		
+		
+	}
+}
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,17 +1,36 @@
 package test;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Test {
 
 	public static void main(String args[]) {
 		
-		ArrayList<Integer> intList = new ArrayList<>(10);
+		LinkedList<String> animal = new LinkedList<>();
+		animal.add("Dog");
+		animal.add("Cat");
+		animal.add("Bird");
 		
-		for(int i = 0 ; i < 7; i++) {
-			intList.add((i + 1) * 5);
-		}
+		System.out.println(animal);
 		
-		System.out.println(intList);
+		addMoreAnimal(animal);
+		
+		System.out.println("The last animal in the list is "+animal.getLast());
+		
+		
 	}
+	
+	private static void addMoreAnimal(LinkedList<String> animal) {
+		animal.add("Fox");
+		animal.add("Salamander");
+		
+		System.out.println(animal);
+		
+	}
+	
+	//private static void getAnimal(LinkedList<String> animal) {
+		
+		//System.out.println("The last animal in the list is: "+animal.getLast());
+	//}
 }
