ef607d245a610fe15e242a847ca841f033570ffa
josefortyfive <jose.fortyfive@gmail.com>
2024-02-09 18:25:34 +0800

    added Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,5 +1,17 @@
 package test;
 
+import java.util.ArrayList;
+
 public class Test {
 
+	public static void main(String args[]) {
+		
+		ArrayList<Integer> intList = new ArrayList<>(10);
+		
+		for(int i = 0 ; i < 7; i++) {
+			intList.add((i + 1) * 5);
+		}
+		
+		System.out.println(intList);
+	}
 }
