8a060917dbb68118bffa479e52e4e87768fa349f
josefortyfive <jose.fortyfive@gmail.com>
2024-02-09 21:00:26 +0800

    LinkedList challenge enchanced

--- src/list/linkedlistchallenge/LinkedListChallenge.java
+++ src/list/linkedlistchallenge/LinkedListChallenge.java
@@ -1,6 +1,7 @@
 package list.linkedlistchallenge;
 
 import java.util.LinkedList;
+import java.util.Scanner;
 
 record Place(String name, int distance) {
 	
@@ -14,6 +15,7 @@ record Place(String name, int distance) {
 public class LinkedListChallenge {
 
 	public static void main(String args[]) {
+		
 		LinkedList<Place> placesToVisit = new LinkedList<>();
 		Place adelaide = new Place("Adelaide",1374);
 		addPlace(placesToVisit, adelaide);
@@ -27,9 +29,65 @@ public class LinkedListChallenge {
 		placesToVisit.addFirst(new Place("Sydney", 0));
 		System.out.println(placesToVisit);
 		
+		var iterator = placesToVisit.listIterator();
+		Scanner scanner = new Scanner(System.in);
+		boolean quitLoop = false;
+		boolean forward = true;
 		
+		printMenu();
+		
+		while(!quitLoop) {
+			if(!iterator.hasPrevious()) {
+				System.out.println("Originating : " +iterator.next());
+				forward = true;
+			}
+			if(!iterator.hasNext()) {
+				System.out.println("Final : " +iterator.previous());
+				forward = false;
+			}
+			System.out.print("Enter value: ");
+			String menuItem = scanner.nextLine().toUpperCase().substring(0, 1);
+			
+			switch(menuItem) {
+			case "F":
+				System.out.println("User wants to go forward");
+				if(!forward) { 				//reverse direction
+					forward = true;
+					if(iterator.hasNext()) {
+						iterator.next(); // Adjust position forward
+					}
+				}
+				if(iterator.hasNext()) {
+					System.out.println(iterator.next());
+				}
+				break;
+			case "B":
+				System.out.println("Uuser wants to go backwards");
+				if(forward) { 				//reverse direction
+					forward = false;
+					if(iterator.hasPrevious()) {
+						iterator.previous(); // Adjust position forward
+					}
+				}
+				if(iterator.hasPrevious()) {
+					System.out.println(iterator.previous());
+				}
+				break;
+			case "M":
+				printMenu();
+				break;
+			case "L":
+				System.out.println(placesToVisit);
+				break;
+			default:
+					quitLoop =true;
+					break;
+			}
+		}
 		
 	}
+	
+	
 	
 	private static void addPlace(LinkedList<Place> list, Place place) {
 		if(list.contains(place)) {
@@ -55,5 +113,14 @@ public class LinkedListChallenge {
 		list.add(place);
 		
 		
+	}
+	private static void printMenu() {
+		System.out.println(""" 
+				Available  actions (select word or letter):
+				(F)orward
+				(B)ackwards
+				(L)ist Places
+				(M)enu
+				(Q)uit""");
 	}
 }
