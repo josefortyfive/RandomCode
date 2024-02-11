945beada28434a9182c82ff75548591c7ede59f5
josefortyfive <jose.fortyfive@gmail.com>
2024-02-11 09:54:57 +0800

    autounboxing example

--- src/list/autoboxing/Autoboxing.java
+++ src/list/autoboxing/Autoboxing.java
@@ -1,5 +1,8 @@
 package list.autoboxing;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Autoboxing {
 
 	public static void main(String[] args) {
@@ -16,9 +19,37 @@ public class Autoboxing {
 		Double resultBoxed = getLiteralDoublePrimitive();
 		double resultUnboxed = getDoubleObject();
 		
+		Integer[] wrapperArray = new Integer[5];
+		wrapperArray[0] = 50;
+		System.out.println(Arrays.toString(wrapperArray));
 		
+		System.out.println(wrapperArray[0].getClass().getName());
+		
+		Character[] characterArray = {'a','b', 'c', 'd'};
+		System.out.println(Arrays.toString(characterArray));	
+		
+		var outList = getList(1, 2, 3,4,5);
+		System.out.println(outList);
 	}
 	
+	
+	
+	private static ArrayList<Integer> getList(int... varargs){
+		ArrayList<Integer> aList = new ArrayList<>();
+		for(int i : varargs) {
+			aList.add(i);
+		}
+		return aList;
+	}
+	
+	private static int returnAnInt(Integer i) {
+		return i;
+	}
+	
+	
+	private static Integer returnAnInteger(int i) {
+		return i;
+	}
 	private static Double getDoubleObject() {
 		return Double.valueOf(100.00);
 	}
