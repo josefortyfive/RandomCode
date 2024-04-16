3471fe16a592e8440468e3ef4ed20127a7435cef
josefortyfive <jose.fortyfive@gmail.com>
2024-04-16 22:06:26 +0800

    Queue sample

---
+++ src/com/linkedin/collections/list/Application.java
@@ -0,0 +1,17 @@
+package com.linkedin.collections.list;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Application {
+	
+	public static void main(String args[]) {
+		List<String> list = new ArrayList<>();
+		list.add("A");
+		list.add("B");
+		list.add("C");
+		list.add("E");
+		list.add("F");
+		System.out.println(list);
+	}
+}
