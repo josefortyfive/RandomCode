7d70c11822c2236140902327114ea480d7d59426
josefortyfive <jose.fortyfive@gmail.com>
2024-04-21 20:21:29 +0800

    Added FindMaxnumber2 implementation

--- src/test/FindMaxNumber.java
+++ src/test/FindMaxNumber.java
@@ -16,11 +16,34 @@ public class FindMaxNumber {
 		return c;
 	}
 	
+	private static int findMaximumNumber2(int a, int b, int c) {
+		int max = a;
+		
+		if(b > max) {
+			max = b;
+		}
+		if(c > max ) {
+			max = c;
+		}
+		
+		return max;
+	}
+	
 	public static void main(String args[]) {
+		
+		// implementation of max number
 		System.out.println(findMaximumNumber(1, 2, 3));
 		System.out.println(findMaximumNumber(5, 20, 33));
 		System.out.println(findMaximumNumber(40, 12, 6));
 		System.out.println(findMaximumNumber(8, 55, 35));
+		
+		// implementation of max number more efficient
+		System.out.println("");
+		
+		System.out.println(findMaximumNumber2(1, 2, 3));
+		System.out.println(findMaximumNumber2(5, 20, 33));
+		System.out.println(findMaximumNumber2(40, 12, 6));
+		System.out.println(findMaximumNumber2(8, 55, 35));
 		
 	}
 }
