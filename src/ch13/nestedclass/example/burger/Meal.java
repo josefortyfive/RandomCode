c447f32a31a6f4c76dc7d5f8c9e1fedea384a8ab
josefortyfive <jose.fortyfive@gmail.com>
2024-05-13 20:56:04 +0800

    modified the ch13.nestclass.example.burger package

--- src/ch13/nestedclass/example/burger/Meal.java
+++ src/ch13/nestedclass/example/burger/Meal.java
@@ -6,8 +6,14 @@ public class Meal {
 	private Item burger;
 	private Item drink;
 	private Item side;
+	private double conversionRate;
 	
 	public Meal() {
+		this(1);
+	}
+	
+	public Meal(double conversionRate) {
+		this.conversionRate = conversionRate;
 		burger = new Item("regular", "burger");
 		drink = new Item("coke ", "drink", 1.5);
 		System.out.println(drink.name);
@@ -15,9 +21,14 @@ public class Meal {
 	}
 	
 	
+	public double getTotal() {
+		double total = burger.price + drink.price + side.price;
+		return Item.getPrice(total, conversionRate);
+	}
+	
 	@Override
 	public String toString() {
-		return "%s%n%s%n%s%n".formatted(burger, drink, side);
+		return "%s%n%s%n%s%n%26s$%.2f".formatted(burger, drink, side, "Total Due: ", getTotal());
 	}
 
 
@@ -40,7 +51,11 @@ public class Meal {
 
 		@Override
 		public String toString() {
-			return "%10s%15s $%.2f".formatted(type, name, price);
+			return "%10s%15s $%.2f".formatted(type, name,getPrice(price, conversionRate));
+		}
+		
+		private static double getPrice(double price, double rate) {
+			return price * rate;
 		}
 		
 		
--- src/ch13/nestedclass/example/burger/Store.java
+++ src/ch13/nestedclass/example/burger/Store.java
@@ -4,5 +4,8 @@ public class Store {
 	public static void main(String args[]) {
 		Meal regularMeal = new Meal();
 		System.out.println(regularMeal);
+		
+		Meal USRegularMeal = new Meal(0.68);
+		System.out.println(USRegularMeal);
 	}
 }
