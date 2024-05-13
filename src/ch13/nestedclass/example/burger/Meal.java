def5b5d3cadafced6ffd433286a2639b8a6b2fed
josefortyfive <jose.fortyfive@gmail.com>
2024-05-13 20:48:54 +0800

    added Meal.java and Store.java

---
+++ src/ch13/nestedclass/example/burger/Meal.java
@@ -0,0 +1,49 @@
+package ch13.nestedclass.example.burger;
+
+public class Meal {
+	
+	private double base = 5.0;
+	private Item burger;
+	private Item drink;
+	private Item side;
+	
+	public Meal() {
+		burger = new Item("regular", "burger");
+		drink = new Item("coke ", "drink", 1.5);
+		System.out.println(drink.name);
+		side = new Item("fries", "side", 2.0);
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "%s%n%s%n%s%n".formatted(burger, drink, side);
+	}
+
+
+	private class Item{
+		private String name;
+		private String type;
+		private double price;
+		public Item(String name, String type) {
+			this(name, type, type.equals("burger") ? base: 0);
+		}
+		
+		
+		public Item(String name, String type, double price) {
+			super();
+			this.name = name;
+			this.type = type;
+			this.price = price;
+		}
+
+
+		@Override
+		public String toString() {
+			return "%10s%15s $%.2f".formatted(type, name, price);
+		}
+		
+		
+		
+	}
+}
---
+++ src/ch13/nestedclass/example/burger/Store.java
@@ -0,0 +1,8 @@
+package ch13.nestedclass.example.burger;
+
+public class Store {
+	public static void main(String args[]) {
+		Meal regularMeal = new Meal();
+		System.out.println(regularMeal);
+	}
+}
