c53af72cb0da9ed034b4ad9de3c7320cb8b202f9
josefortyfive <jose.fortyfive@gmail.com>
2024-05-13 21:12:53 +0800

    added the Meal, Burger challenge

--- src/ch13/nestedclass/example/burger/Store.java
+++ src/ch13/nestedclass/example/burger/Store.java
@@ -3,6 +3,7 @@ package ch13.nestedclass.example.burger;
 public class Store {
 	public static void main(String args[]) {
 		Meal regularMeal = new Meal();
+		regularMeal.addToppings("Ketchup", "Mayo", "Bacon", "Cheddar");
 		System.out.println(regularMeal);
 		
 		Meal USRegularMeal = new Meal(0.68);
