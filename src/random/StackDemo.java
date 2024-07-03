415b2a40cdf6c5caacd5b061c30eea767daf067e
josefortyfive <jose.fortyfive@gmail.com>
2024-07-03 21:43:45 +0800

    Demo Stack

---
+++ src/random/StackDemo.java
@@ -0,0 +1,32 @@
+package random;
+
+import java.io.*;
+import java.util.*;
+
+class StackDemo {
+	  
+    // Main Method
+  public static void main(String[] args)
+  {
+
+      // Default initialization of Stack
+      Stack<String> stack1 = new Stack<String>();
+
+      // Initialization of Stack
+      // using Generics
+      Stack<String> stack2 = new Stack<String>();
+
+      // pushing the elements
+      stack1.push("4");
+      stack1.push("All");
+      stack1.push("Geeks");
+
+      stack2.push("Geeks");
+      stack2.push("For");
+      stack2.push("Geeks");
+
+        // Printing the Stack Elements
+      System.out.println(stack1);
+      System.out.println(stack2);
+  }
+}
