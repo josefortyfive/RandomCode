c2bd99049d1ef4b5b9e2fe03ea65e804b02fb2f4
josefortyfive <jose.fortyfive@gmail.com>
2024-07-23 22:03:05 +0800

    QueueExample5

---
+++ src/com/queue/QueueExample5.java
@@ -0,0 +1,22 @@
+package com.queue;
+
+
+import java.util.*;
+
+public class QueueExample5 {
+
+    public static void main(String args[])
+    {
+        Queue<String> pq = new PriorityQueue<>();
+
+        pq.add("Geeks");
+        pq.add("For");
+        pq.add("Geeks");
+
+        Iterator iterator = pq.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+    }
+}
