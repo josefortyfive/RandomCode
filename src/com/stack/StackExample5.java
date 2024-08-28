b218e40168eede59fdaeb4b4df048c8e6981a767
josefortyfive <jose.fortyfive@gmail.com>
2024-08-28 22:06:11 +0800

    StackExample5

---
+++ src/com/stack/StackExample5.java
@@ -0,0 +1,22 @@
+package com.stack;
+
+import java.util.Stack;
+import java.util.stream.Stream;
+
+public class StackExample5 {
+
+	public static void main(String[] args) {
+		Stack<String> stack = new Stack<String>();
+		stack.push("A");
+		stack.push("B");
+		stack.push("C");
+
+		Stream<String> stream = stack.stream();
+
+		stream.forEach((element) -> {
+		    System.out.println(element);  // print element
+		});
+
+	}
+
+}
