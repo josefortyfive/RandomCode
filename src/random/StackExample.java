7fa69fa4f816faacd055b560d6afb3c3e760c584
josefortyfive <jose.fortyfive@gmail.com>
2024-09-27 22:40:14 +0800

    Modfied stack sample

--- src/random/StackExample.java
+++ src/random/StackExample.java
@@ -5,9 +5,11 @@ public class StackExample {
 	public static void main(String[] args) {
 		Stack<String> sample = new Stack<>();
 		
-		sample.push("JAck");
+		sample.push("Jack");
 		sample.push("Amy");
 		sample.push("John");
+		sample.push("Nancy");
+		sample.push("Binay");
 		
 		while(!sample.isEmpty()) {
 
