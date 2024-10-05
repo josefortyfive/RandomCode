be9cabeeac1e5850380178db6f29d8646c516173
josefortyfive <jose.fortyfive@gmail.com>
2024-10-05 22:55:29 +0800

    SetExample4

---
+++ src/com/set/SetExample4.java
@@ -0,0 +1,23 @@
+package com.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetExample4 {
+
+	public static void main(String args[]) {
+        // Creating an object of Set and 
+        // declaring object of type String
+        Set<String> hs = new HashSet<String>();
+
+        // Adding elements to above object
+        // using add() method
+        hs.add("B");
+        hs.add("B");
+        hs.add("C");
+        hs.add("A");
+
+        // Printing the elements inside the Set object
+        System.out.println(hs);
+	}
+}
