17b9beda52bdf05bf5bed449e3644bea8ac039e5
josefortyfive <jose.fortyfive@gmail.com>
2024-12-24 22:11:20 +0800

    added new sample BigDecimal

--- src/practice/BigDecimalPractice.java
+++ src/practice/BigDecimalPractice.java
@@ -5,14 +5,21 @@ import java.math.BigDecimal;
 public class BigDecimalPractice {
 
 	public static void main(String args[]) {
-		BigDecimal p1 = new BigDecimal("0.1");
-		BigDecimal p2 = new BigDecimal("0.2");
+		
+		double a1 = 0.02;
+		double a2 = 0.03;
+		double a3 = a2 - a1;
+		
+		BigDecimal p1 = new BigDecimal("0.02");
+		BigDecimal p2 = new BigDecimal("0.03");
 		
 		BigDecimal p3;
 		
-		p3 = p1.subtract(p2);
+		p3 = p2.subtract(p1);
 		
 		System.out.println(p3);
+		
+		System.out.println(a3);
 		
 	}
 }
